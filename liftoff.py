6d08374e50d43f76c8dd5a03a33b6637fc11df2f
Jaime Rogstad <66330786+jrog20@users.noreply.github.com>
2021-04-30 08:48:20 -0400

    Update liftoff.py

--- liftoff.py
+++ liftoff.py
@@ -6,14 +6,10 @@ It counts down the numbers from 10 to 1 and then writes “Liftoff!”
 """
 
 def main():
-    """
-    Using a for loop, this program will count down from 10 to 1.
-    """
-
     for i in range(10, 0, -1):
         print(i)
     print("Liftoff!")
-
+    
 
 if __name__ == "__main__":
     main()
